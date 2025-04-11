42a5a0e50b812aa1850dc3b7d6322f1e131fba1a
Siddharth Makwana <112619536+sidmakwana11@users.noreply.github.com>
2025-04-11 13:35:51 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,9 @@ exports.signup = async (req, res) => {
   let { username, email, password } = req.body;
   if (!username || !email || !password) return res.status(400).json({ error: "All fields are required" });
 
-  Email = email.toLowerCase();
+  email = email.toLowerCase();
   
-  const existingUser = await User.findOne({ Email });
+  const existingUser = await User.findOne({ email });
   if (existingUser) return res.status(400).json({ error: "Email already registered" });
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -21,9 +21,9 @@ exports.login = async (req, res) => {
   let { email, password } = req.body;
   if (!email || !password) return res.status(400).json({ error: "Email and password are required" });
 
-  Email = email.toLowerCase();
+  email = email.toLowerCase();
   
-  const user = await User.findOne({ Email });
+  const user = await User.findOne({ email });
   if (!user || !(await bcrypt.compare(password, user.password)))
     return res.status(401).json({ error: "Invalid email or password" });
 
